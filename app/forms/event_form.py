15f7c21f6f224d88b7f5ae32f499f97c96a46641
MasonKogami <maskogami@gmail.com>
2022-05-10 14:32:05 -0700

    add event seed data

--- app/forms/event_form.py
+++ app/forms/event_form.py
@@ -5,11 +5,11 @@ from wtforms.validators import DataRequired
 
 class EventForm(FlaskForm):
   name             = StringField('name', validators=[DataRequired()])
-  host_id          = IntegerField('host', validators=[DataRequired()])
+  user_id          = IntegerField('host', validators=[DataRequired()])
+  category_id      = IntegerField('category')
   location_name    = StringField('location name', validators=[DataRequired()])
   address          = StringField('address', validators=[DataRequired()])
   city             = StringField('city', validators=[DataRequired()])
   state            = StringField('state', validators=[DataRequired()])
-  category_id      = IntegerField('category')
   date             = DateTimeField('date', validators=[DataRequired()])
   capacity         = IntegerField('capacity', validators=[DataRequired()])
--- app/seeds/events.py
+++ app/seeds/events.py
@@ -1,9 +1,13 @@
 from app.models import db, User, Event
 
-# def seed_events():
-#   event1 = Event(user_id=1, venue_id=)
-#   event2 = Event(user_id=2, )
-#   event3 = Event(user_id=3, )
-#   event4 = Event(user_id=1, )
-#   event5 = Event(user_id=2, )
-#   event6 = Event(user_id=3, )
+def seed_events():
+  event1 = Event(user_id=1, name='FoodieLand Night Market', location_name= 'Golden Gate Fields', address= '', city= '', state= '', date= '', capacity=10000)
+  event2 = Event(user_id=2, name='BottleRock 2022', location_name= 'Napa Valley Expo', address= '', city= '', state= '', date= '', capacity=5000)
+  event3 = Event(user_id=3, name='Day N Vegas', location_name= 'Las Vegas Festival Grounds', address= '', city= '', state= '', date= '', capacity=50000)
+  event4 = Event(user_id=1, name='Grad Nite', location_name= 'Disneyland', address= '', city= '', state= '', date= '', capacity=500)
+  event5 = Event(user_id=2, name='Wine N Dine', location_name= 'Napa Valley Expo', address= '', city= '', state= '', date= '', capacity=100)
+  event6 = Event(user_id=3, name='Skydiving', location_name= '', address= '', city= '', state= '', date= '', capacity=20)
+
+def undo_events():
+    db.session.execute('TRUNCATE boards RESTART IDENTITY CASCADE;')
+    db.session.commit()
