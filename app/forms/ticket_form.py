ade2e6d5ec41e4765387ac5151e92191573f9580
MasonKogami <maskogami@gmail.com>
2022-05-10 16:37:54 -0700

    add ticket form

---
+++ app/forms/ticket_form.py
@@ -0,0 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import IntegerField
+from wtforms.validators import DataRequired
+
+
+class TicketForm(FlaskForm):
+  event_id = IntegerField('event', validators=[DataRequired()])
+  user_id  = IntegerField('user', validators=[DataRequired()])
