da6c8165a864b349cf65a9cff1af7f99e48e1636
MasonKogami <maskogami@gmail.com>
2022-05-11 08:44:29 -0700

    add events store file, add event constants

---
+++ react-app/src/store/events.js
@@ -0,0 +1,12 @@
+// C O N S T A N T S
+const CREATE_EVENT = 'events/CREATE_EVENT'
+const READ_ALL_EVEVNTS = 'events/READ_ALL_EVENTS'
+const READ_ONE_EVENT = 'events/READ_ONE_EVENT'
+const UPDATE_EVENT = 'events/UPDATE_EVENT'
+const DELETE_EVENT = 'events/DELETE_EVENT'
+
+// A C T I O N S
+
+// T H U N K S 
+
+// R E D U C E R
