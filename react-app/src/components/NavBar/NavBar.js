02fce4e21336b9cbdccb7bb6db2d047a4409e815
MasonKogami <maskogami@gmail.com>
2022-05-11 15:11:48 -0700

    add layout for navbar

--- react-app/src/components/NavBar/NavBar.js
+++ react-app/src/components/NavBar/NavBar.js
@@ -6,35 +6,33 @@ import './NavBar.css';
 const NavBar = () => {
   return (
     <nav>
-      <ul>
-        <div>
-          <li>
-            <NavLink to='/' exact={true} activeClassName='active'>
-              Home
-            </NavLink>
-          </li>
-        </div>
-        <div>
-          <li>
-            <NavLink to='/login' exact={true} activeClassName='active'>
-              Login
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to='/sign-up' exact={true} activeClassName='active'>
-              Sign Up
-            </NavLink>
-          </li>
-        </div>
-        {/* <li>
-          <NavLink to='/users' exact={true} activeClassName='active'>
-            Users
+      <div className='home'>
+          <NavLink to='/' exact={true} activeClassName='active' style={{textDecoration: 'none'}}>
+            Home
           </NavLink>
-        </li> */}
-        <li>
-          <LogoutButton />
-        </li>
-      </ul>
+          <div>
+            Create Event
+          </div>
+      </div>
+      {/* <div className='login-signup'>
+          <NavLink to='/login' exact={true} activeClassName='active' style={{textDecoration: 'none'}}>
+            Login
+          </NavLink>
+          <NavLink to='/sign-up' exact={true} activeClassName='active' style={{textDecoration: 'none'}}>
+            Sign Up
+          </NavLink>
+      </div> */}
+      {/* <li>
+        <NavLink to='/users' exact={true} activeClassName='active'>
+          Users
+        </NavLink>
+      </li> */}
+      <div className='logout-ticket'>
+        <div>
+          Tickets
+        </div>
+        <LogoutButton />
+      </div>
     </nav>
   );
 }
