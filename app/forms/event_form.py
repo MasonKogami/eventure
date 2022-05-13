0b1af4ad308eb9fa22f060a53cba41478d3fe020
MasonKogami <maskogami@gmail.com>
2022-05-12 20:53:35 -0700

    update event model, form, seed data; combined address, city, state into one string

--- app/forms/event_form.py
+++ app/forms/event_form.py
@@ -9,7 +9,5 @@ class EventForm(FlaskForm):
   category_id      = IntegerField('category')
   location_name    = StringField('location name', validators=[DataRequired()])
   address          = StringField('address', validators=[DataRequired()])
-  city             = StringField('city', validators=[DataRequired()])
-  state            = StringField('state', validators=[DataRequired()])
   date             = DateTimeField('date', validators=[DataRequired()])
   capacity         = IntegerField('capacity', validators=[DataRequired()])
--- app/seeds/events.py
+++ app/seeds/events.py
@@ -1,12 +1,12 @@
 from app.models import db, User, Event
 
 def seed_events():
-  event1 = Event(user_id=1, name='FoodieLand Night Market', location_name= 'Golden Gate Fields', address= '1100 Eastshore Hwy,', city= 'Berkeley', state= 'CA', date= '2022-06-21', capacity=10000)
-  event2 = Event(user_id=2, name='BottleRock 2022', location_name= 'Napa Valley Expo', address= '575 3rd St', city= 'Napa', state= 'CA', date= '2022-06-21', capacity=5000)
-  event3 = Event(user_id=3, name='Day N Vegas', location_name= 'Las Vegas Festival Grounds', address= '311 W Sahara Ave', city= 'Las Vegas', state= 'NV', date= '2022-06-21', capacity=50000)
-  event4 = Event(user_id=1, name='Grad Nite', location_name= 'Disneyland', address= '1313 Disneyland Dr,', city= 'Anaheim', state= 'CA', date= '2022-06-21', capacity=500)
-  event5 = Event(user_id=2, name='Wine N Dine', location_name= 'Napa Valley Expo', address= '575 3rd St', city= 'Napa', state= 'CA', date= '2022-06-21', capacity=100)
-  event6 = Event(user_id=3, name='Skydiving', location_name= 'Skydive Golden Gate', address= '351 Airport Rd', city= 'Novato', state= 'CA', date= '2022-06-21', capacity=20)
+  event1 = Event(user_id=1, name='FoodieLand Night Market', location_name= 'Golden Gate Fields', address= '1100 Eastshore Hwy, Berkeley, CA', date= '2022-06-21', capacity=10000)
+  event2 = Event(user_id=2, name='BottleRock 2022', location_name= 'Napa Valley Expo', address= '575 3rd St, Napa, CA', date= '2022-06-21', capacity=5000)
+  event3 = Event(user_id=3, name='Day N Vegas', location_name= 'Las Vegas Festival Grounds', address= '311 W Sahara Ave, Las Vegas, NV', date= '2022-06-21', capacity=50000)
+  event4 = Event(user_id=1, name='Grad Nite', location_name= 'Disneyland', address= '1313 Disneyland Dr, Anaheim, CA', date= '2022-06-21', capacity=500)
+  event5 = Event(user_id=2, name='Wine N Dine', location_name= 'Napa Valley Expo', address= '575 3rd St, Napa, CA', date= '2022-06-21', capacity=100)
+  event6 = Event(user_id=3, name='Skydiving', location_name= 'Skydive Golden Gate', address= '351 Airport Rd, Novato, CA', date= '2022-06-21', capacity=20)
 
   db.session.add_all([event1, event2, event3, event4, event5, event6])
   db.session.commit()
