eaa95930cc6dbf4ba2de7c4151d65ab1cc1f9f6e
MasonKogami <maskogami@gmail.com>
2022-05-13 16:41:50 -0700

    fix date form validations

--- app/forms/event_form.py
+++ app/forms/event_form.py
@@ -4,10 +4,11 @@ from wtforms.validators import DataRequired
 
 
 class EventForm(FlaskForm):
+  # pass
   name             = StringField('name', validators=[DataRequired()])
   user_id          = IntegerField('host', validators=[DataRequired()])
   category_id      = IntegerField('category')
   location_name    = StringField('location name', validators=[DataRequired()])
   address          = StringField('address', validators=[DataRequired()])
-  date             = DateTimeField('date', validators=[DataRequired()])
+  date             = StringField('date', validators=[DataRequired()])
   capacity         = IntegerField('capacity', validators=[DataRequired()])
