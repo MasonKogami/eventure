a00383fad2d00c3e431fc496c471f51db0cb0cfe
MasonKogami <maskogami@gmail.com>
2022-05-14 21:51:03 -0700

    modify one event component

--- react-app/src/components/Events/OneEvent.js
+++ react-app/src/components/Events/OneEvent.js
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams, useHistory } from 'react-router-dom';
-import { readAllEvents, readOneEvent } from '../../store/events';
+import { readOneEvent } from '../../store/events';
+import './OneEvent.css';
 
 const OneEvent = () => {
   const dispatch = useDispatch();
   const history = useHistory();
   const sessionUser = useSelector(state => state.session.user);
   const { eventId } = useParams();
-  const events = useSelector(state => state.events);
   const event = useSelector(state => state.events[eventId]);
   // console.log(events)
   // console.log(event);
@@ -19,17 +19,19 @@ const OneEvent = () => {
 
   return (
     <div>
-      <div>
-        {event?.name}
-      </div>
-      <div>
-        {event?.date}
+      <div className='bg-image-header'></div>
+      <div className='header-div'>
+        <div>
+          <img src='https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F230834389%2F285623250502%2F1%2Foriginal.20210604-004626?w=600&auto=format%2Ccompress&q=75&sharp=10&rect=0%2C0%2C2160%2C1080&s=f94e8cdbce75497cb7ede588ea34da22' />
+        </div>
+        <div>
+          {event?.name}
+          {event?.date}
+        </div>
       </div>
       <div>
         {event?.location_name}
-      </div>
-      <div>
-      {event?.address}
+        {event?.address}
       </div>
     </div>
   );
