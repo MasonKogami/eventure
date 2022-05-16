90dbd52c7ac376a0bc5e0c28749bd6d43ae5c085
MasonKogami <maskogami@gmail.com>
2022-05-16 14:47:45 -0700

    add alt to landing page image

--- react-app/src/components/LandingPage/LandingPage.js
+++ react-app/src/components/LandingPage/LandingPage.js
@@ -1,13 +1,13 @@
 // import { Parallax, ParallaxLayer } from '@react-spring/parallax';
 
 const image1 = 'https://images.pexels.com/photos/167491/pexels-photo-167491.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-const image2 = 'https://images.pexels.com/photos/1105666/pexels-photo-1105666.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+// const image2 = 'https://images.pexels.com/photos/1105666/pexels-photo-1105666.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
 
 const LandingPage = () => {
 
   return (
     <div style={{width: '100%'}}>
-      <img src={`${image1}`} style={{backgroundSize: 'cover'}}/>
+      <img alt='landingpage-header' src={`${image1}`} style={{backgroundSize: 'cover'}}/>
     </div>
   )
 };
