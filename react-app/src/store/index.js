96e7a1a1df448216e5a4ec5b674852b89e899030
MasonKogami <maskogami@gmail.com>
2022-05-16 21:54:50 -0700

    import ticketsReducer into rootReducer

--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -2,10 +2,12 @@ import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import session from './session'
 import eventsReducer from './events';
+import ticketsReducer from './tickets';
 
 const rootReducer = combineReducers({
   session,
-  events: eventsReducer
+  events: eventsReducer,
+  tickets: ticketsReducer
 });
 
 
