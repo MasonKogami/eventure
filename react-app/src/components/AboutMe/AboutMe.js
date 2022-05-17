09ca79688f76c9c3c8a647841b5f3c3de59dc4d1
MasonKogami <maskogami@gmail.com>
2022-05-16 23:50:24 -0700

    add about me section footer, WIP

--- react-app/src/components/AboutMe/AboutMe.js
+++ react-app/src/components/AboutMe/AboutMe.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './AboutMe.css';
+import { FaGithub, FaLinkedin } from 'react-icons/fa'
 
 const AboutMe = () => {
 
   return (
-    <>
-    </>
+    <div className='aboutme-section'>
+      <p style={{color: 'black'}}>© 2022 Eventure</p>
+      <a href='https://github.com/MasonKogami' style={{textDecoration: 'none'}}>
+        <FaGithub size={30} style={{textDecoration: 'none', color: 'black'}}/>
+      </a>
+      <a href='https://www.linkedin.com/in/mason-kogami-74b789231/'>
+        <FaLinkedin size={30} style={{color: '#0a66c2'}}/>
+      </a>
+    </div>
   );
 };
 
