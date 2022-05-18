2a251da955940594f3e8f6ec16b333c70f594597
MasonKogami <maskogami@gmail.com>
2022-05-17 22:51:24 -0700

    add log in and signup modal buttons, signup modal bug: modal will not close

--- react-app/src/components/LandingPage/LandingPage.js
+++ react-app/src/components/LandingPage/LandingPage.js
@@ -1,14 +1,56 @@
-// import { Parallax, ParallaxLayer } from '@react-spring/parallax';
-
-const image1 = 'https://images.pexels.com/photos/167491/pexels-photo-167491.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-// const image2 = 'https://images.pexels.com/photos/1105666/pexels-photo-1105666.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import './LandingPage.css';
+import LoginForm from '../auth/LoginForm';
+import SignUpForm from '../auth/SignUpForm';
+import Modal from '../Modal/Modal';
 
 const LandingPage = () => {
+  const dispatch = useDispatch();
+
+  const [loginDisplay, setLoginDisplay] = useState('not-displayed')
+  const [signupDisplay, setSignupDisplay] = useState('not-displayed')
+
+  const changeLoginDisplay = () => {
+    if (loginDisplay === 'not-displayed') {
+        setLoginDisplay('displayed')
+    } else if (loginDisplay === 'displayed') {
+        setLoginDisplay('not-displayed')
+    } else if (signupDisplay === 'displayed') {
+        setLoginDisplay('not-displayed')
+    }
+  };
+
+  const changeSignupDisplay = () => {
+    if (signupDisplay === 'not-displayed') {
+        setSignupDisplay('displayed')
+    } else if (signupDisplay === 'displayed') {
+        setSignupDisplay('not-displayed')
+    } else if (loginDisplay === 'displayed') {
+        setSignupDisplay('not-displayed')
+    }
+  };
+
+  const toggleLoginSignupFunc = () => {
+    changeLoginDisplay();
+    changeSignupDisplay();
+  };
 
   return (
-    <div style={{width: '100%'}}>
-      <img alt='landingpage-header' src={`${image1}`} style={{backgroundSize: 'cover'}}/>
-    </div>
+    <>
+      <h2>Eventure</h2>
+      <div className='landingpage-slideshow' />
+      <div className='user-forms'>
+        <button onClick={changeLoginDisplay}>Log In</button>
+        <button onClick={changeSignupDisplay}>Sign Up</button>
+        <div className={`modal-background-home-page ${loginDisplay}`} onMouseDown={changeLoginDisplay}>
+          <LoginForm closeModalFunc={changeLoginDisplay} toggleLoginSignupFunc={toggleLoginSignupFunc}/>
+        </div>
+        <div className={`modal-background-home-page ${signupDisplay}`}>
+          <SignUpForm closeModalFunc={changeSignupDisplay} toggleLoginSignupFunc={toggleLoginSignupFunc}/>
+        </div>
+      </div>
+    </>
   )
 };
 
