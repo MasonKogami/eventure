1725167d7888b2a3c5cb5ffc9ca72b90bf4d6e8f
MasonKogami <maskogami@gmail.com>
2022-05-22 23:46:40 -0700

    save changes in User.js

--- react-app/src/components/User.js
+++ react-app/src/components/User.js
@@ -52,7 +52,7 @@ function User() {
       <div>
         {tickets?.map((ticket) => {
           return (
-            <SingleTicket key={ticket.id} ticket={ticket} userId={userId} event={Event}/>
+            <SingleTicket key={ticket.id} ticket={ticket} userId={userId} ticketEvent={ticket.event_id}/>
           )
         })}
       </div>
