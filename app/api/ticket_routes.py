c9936df1651a05f759b6a5344b704199ce480377
MasonKogami <maskogami@gmail.com>
2022-05-22 21:56:19 -0700

    add edit ticket form, modify update tickets route

--- app/api/ticket_routes.py
+++ app/api/ticket_routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request
 from flask_login import current_user, login_required
 from app.models import db, Event, Ticket
+from app.forms import EditTicketForm
 
 ticket_routes = Blueprint('tickets', __name__)
 
@@ -10,12 +11,18 @@ ticket_routes = Blueprint('tickets', __name__)
 def update_tickets(id):
   # db query to grab all tickets for the current user
   ticket = Ticket.query.get(id)
+  form = EditTicketForm()
 
-  tickets.quantity = request.json['quantity']
+  form['csrf_token'].data = request.cookies['csrf_token']
+  if form.validate_on_submit():
+    ticket.quantity = request.json['quantity']
 
-  db.session.commit()
+    db.session.add(ticket)
+    db.session.commit()
 
-  return ticket.to_dict()
+    return ticket.to_dict()
+  else:
+    return {'errors': validation_errors_to_error_messages(form.errors)}, 401
   
 # D E L E T E  T I C K E T S
 @ticket_routes.route('/<int:id>', methods=['DELETE'])
---
+++ app/forms/edit_ticket_form.py
@@ -0,0 +1,21 @@
+from flask_wtf import FlaskForm
+from wtforms import IntegerField, StringField
+from wtforms.validators import DataRequired
+from app.models import Event
+
+
+# def ticket_total(form, field):
+#   quantity = field.data
+#   event_id = field.data
+#   print(event_id, "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
+#   event = Event.query.get(+event_id)
+#   print(event, "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
+#   if quantity > event.capacity:
+#     raise ValidationError("You cannot acquire more tickets than the event capacity currently allows.")
+
+
+class EditTicketForm(FlaskForm):
+  event_name = StringField('event name', validators=[DataRequired()])
+  event_id   = IntegerField('event', validators=[DataRequired()])
+  user_id    = IntegerField('user', validators=[DataRequired()])
+  quantity   = IntegerField('quantity', validators=[DataRequired()])
