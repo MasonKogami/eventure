0100ae260e00276444220c8d6710f15553793115
MasonKogami <maskogami@gmail.com>
2022-05-23 00:17:14 -0700

    add user profile title styles

--- react-app/src/components/User.js
+++ react-app/src/components/User.js
@@ -3,28 +3,20 @@ import { useSelector, useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { readAllEvents } from '../store/events';
 import { loadTickets } from '../store/tickets';
-// import { loadUser } from '../store/session';
 import SingleTicket from './Tickets/SingleTicket';
+import { FaUser } from 'react-icons/fa';
 import './User.css';
 
 function User() {
   const dispatch = useDispatch();
   const [user, setUser] = useState({});
   const { userId }  = useParams();
-  // const events = useSelector(state => state.events);
-  // const test = useSelector(state => state.session.user);
   const tickets = useSelector(state => Object.values(state.tickets));
-  // const tickets = user?.tickets;
-  // const tickets = useSelector(state => state.session.user.tickets);
 
   useEffect(() => {
     dispatch(loadTickets(userId));
     dispatch(readAllEvents())
   }, [dispatch, userId]);
-
-  // useEffect(() => {
-  //   dispatch(loadUser(user));
-  // }, [dispatch, user]);
 
   useEffect(() => {
     if (!userId) {
@@ -43,11 +35,12 @@ function User() {
 
   return (
     <div className='user-profile-con'>
-      <h2 className='user-title'>{user.username}'s Tickets</h2>
-      <div className='table-headers'>
-        <h2>Event Name</h2>
-        <h2>Ticket Quantity</h2>
-        <h2>Actions</h2>
+      <div className='user-title'>
+        <FaUser style={{height: '50px'}}/>
+        <h2>{user.username}</h2>
+      </div>
+      <div className='orders'>
+        <h2>Orders</h2>
       </div>
       <div>
         {tickets?.map((ticket) => {
