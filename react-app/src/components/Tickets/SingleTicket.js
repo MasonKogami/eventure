3d2440f7925c670e5eb264d93c205a580388d40c
MasonKogami <maskogami@gmail.com>
2022-06-17 22:17:49 -0700

    begin remodel for single ticket page, add back to orders button with react icon

--- react-app/src/components/Tickets/SingleTicket.js
+++ react-app/src/components/Tickets/SingleTicket.js
@@ -1,14 +1,30 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { deleteTickets } from '../../store/tickets';
 import EditTickets from "./EditTickets";
 import Modal from '../Modal/Modal';
 import ConfirmationModal from "../Modal/Confirmation";
+import { loadTickets } from '../../store/tickets';
+import { readAllEvents } from '../../store/events';
 import'./SingleTicket.css';
+import { useHistory, useParams } from 'react-router-dom';
+import  { MdKeyboardBackspace } from 'react-icons/md';
 
-const SingleTicket = ({ ticketEvent, ticket }) => {
+const SingleTicket = () => {
   const dispatch = useDispatch();
+  const history = useHistory();
+  const { ticketId } = useParams();
+  const sessionUser = useSelector(state => state.session.user);
+  const tickets = useSelector(state => Object.values(state.tickets));
+  const ticket = tickets.find(ticket => ticket.id === +ticketId);
   const [showCheckoutModal, setCheckoutModal] = useState(false);
+  // console.log(tickets);
+  // console.log(ticket);
+
+  useEffect(() => {
+    dispatch(loadTickets(sessionUser.id));
+    dispatch(readAllEvents())
+  }, [dispatch, sessionUser.id]);
   
   const removeTickets = async (ticket) => {
     await dispatch(deleteTickets(ticket));
@@ -18,32 +34,45 @@ const SingleTicket = ({ ticketEvent, ticket }) => {
   const closeCheckoutModalFunc = () => setCheckoutModal(false);
 
   return (
-    <div className='singleticket-con'>
-      <div className='ticket-name'>
-        {ticket.event_name}
+    <div className='single-ticket-page'>
+      <div
+        type='button'
+        className='back-to-orders'
+        onClick={() => history.push(`/users/${ticket?.user_id}`)}
+      >
+      <MdKeyboardBackspace style={{marginRight: '5px'}}/>
+        Back to Orders
       </div>
-      <div className='ticket-quantity'>
-        {ticket.quantity}
-      </div>
-      <div className='refund-tickets'>
-        {<ConfirmationModal 
-                message="Are you sure you want to refund these tickets?"
-                actionButtonLabel="Refund Tickets"
-                func={() => removeTickets(ticket)}
-              >
-            <button
-              style={{ fontSize: '12px', cursor: 'pointer', backgroundColor: '#d1410c', color: '#ffff', borderRadius: '4px', border: '1px solid #d1410c', height: '30px', width: '110px', marginTop: '10px'}}
-            >Refund Tickets</button>
-          </ConfirmationModal>
-        }
-      </div>
-      <div className='update-tickets'>
-        {<button style={{ fontSize: '12px', cursor: 'pointer', backgroundColor: '#d1410c', color: '#ffff', borderRadius: '4px', border: '1px solid #d1410c', height: '30px', width: '110px', marginTop: '10px'}} onClick={showCheckoutModalFunc}>Update Tickets</button>}
-          {showCheckoutModal && (
-            <Modal closeModalFunc={closeCheckoutModalFunc} className='modal-background-edit'>
-              <EditTickets ticketEvent={ticketEvent} ticket={ticket} style={{justifyContent: 'center'}} closeModalFunc={closeCheckoutModalFunc} />
-            </Modal>
-          )}
+      <div className='singleticket-con'>
+        <div className='ticket-name'>
+          {ticket?.event_name}
+        </div>
+        <div className='ticket-quantity'>
+          {ticket?.quantity}
+        </div>
+        <div className='refund-tickets'>
+          {<ConfirmationModal 
+                  message="Are you sure you want to refund these tickets?"
+                  actionButtonLabel="Refund Tickets"
+                  func={() => {
+                    removeTickets(ticket)
+                    history.push(`/users/${ticket?.user_id}`)
+                  }}
+                >
+              <button
+                style={{ fontSize: '12px', cursor: 'pointer', backgroundColor: '#d1410c', color: '#ffff', borderRadius: '4px', border: '1px solid #d1410c', height: '30px', width: '110px', marginTop: '10px'}}
+              >Refund</button>
+            </ConfirmationModal>
+          }
+        </div>
+        <div className='update-tickets'>
+          {<button style={{ fontSize: '12px', cursor: 'pointer', backgroundColor: '#d1410c', color: '#ffff', borderRadius: '4px', border: '1px solid #d1410c', height: '30px', width: '110px', marginTop: '8px'}} onClick={showCheckoutModalFunc}>Update</button>}
+            {showCheckoutModal && (
+              <Modal closeModalFunc={closeCheckoutModalFunc} className='modal-background-edit'>
+                <EditTickets ticket={ticket} style={{justifyContent: 'center'}} closeModalFunc={closeCheckoutModalFunc} />
+              </Modal>
+            )}
+        </div>
       </div>
     </div>
   );
