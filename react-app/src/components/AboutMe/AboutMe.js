8a9b96928276655015962454ad4a1d879a95520b
MasonKogami <maskogami@gmail.com>
2022-07-05 21:27:57 -0700

    about me links open new tab

--- react-app/src/components/AboutMe/AboutMe.js
+++ react-app/src/components/AboutMe/AboutMe.js
@@ -7,10 +7,10 @@ const AboutMe = () => {
   return (
     <div className='aboutme-section'>
       <p style={{color: 'whitesmoke'}}>© 2022 Eventure</p>
-      <a href='https://github.com/MasonKogami' style={{textDecoration: 'none'}}>
+      <a target='_blank' href='https://github.com/MasonKogami' style={{textDecoration: 'none'}}>
         <FaGithub size={30} style={{textDecoration: 'none', color: 'whitesmoke'}}/>
       </a>
-      <a href='https://www.linkedin.com/in/mason-kogami-74b789231/'>
+      <a target='_blank' href='https://www.linkedin.com/in/masonkogami/'>
         <FaLinkedin size={30} style={{color: '#0a66c2'}}/>
       </a>
     </div>
