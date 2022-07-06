057484ee0ff6fe307d87a832756ad490900d1ce0
MasonKogami <maskogami@gmail.com>
2022-07-06 09:54:42 -0700

    adjust dropdown menu

--- react-app/src/components/NavBar/NavBar.js
+++ react-app/src/components/NavBar/NavBar.js
@@ -28,24 +28,7 @@ const NavBar = () => {
           <NavLink to='/home' exact={true} activeClassName='active' style={{textDecoration: 'none', marginLeft: '24px', color: '#d1410c', fontSize: '24px', fontWeight: 'bolder'}}>
             Eventure
           </NavLink>
-          {/* <NavLink to='/events/create' exact={true} style={{textDecoration: 'none', verticalAlign: 'end', color: 'rgb(61, 100, 255)'}}>
-            <BsPlusLg style={{ position: 'relative', bottom: '15px', left: '55px'}}/>
-            Create Event
-          </NavLink> */}
       </div>
-      {/* <div className='login-signup'>
-          <NavLink to='/login' exact={true} activeClassName='active' style={{textDecoration: 'none'}}>
-            Login
-          </NavLink>
-          <NavLink to='/sign-up' exact={true} activeClassName='active' style={{textDecoration: 'none'}}>
-            Sign Up
-          </NavLink>
-      </div> */}
-      {/* <li>
-        <NavLink to='/users' exact={true} activeClassName='active'>
-          Users
-        </NavLink>
-      </li> */}
       <div className='logout-ticket' style={{color: '#39364f'}}>
         <div className='create-event-con'>
           <button 
@@ -56,10 +39,6 @@ const NavBar = () => {
             <FaRegPlusSquare style={{ position: 'relative', bottom: '1px', left: '-1px'}}/>
             Create an event</button>
         </div>
-        {/* <NavLink to='/events/create' exact={true} style={{position: 'relative', top: '10px', textDecoration: 'none', verticalAlign: 'end', color: 'rgb(61, 100, 255)', fontSize: '14px'}}>
-          <FaRegPlusSquare style={{ position: 'relative', bottom: '15px', left: '58px'}}/>
-          Create an event
-        </NavLink> */}
         <div className='likes'>
           <button className='likes-btn'>
             <FaRegHeart style={{position: 'relative', top: '-2px', left: '1px', fontWeight: 'bolder'}}/>
@@ -75,21 +54,29 @@ const NavBar = () => {
           >
             <FaTicketAlt style={{position: 'relative', bottom: '3px', left: '1px'}} />
             Tickets</button>
-          {/* <NavLink className='tickets-link' to={`/users/${sessionUser.id}`}>
-            Tickets</NavLink> */}
         </div>
         <div className='dropdown'>
           <button className='dropdown-btn'>
             <FaUser style={{height: '16px', width: '16px', marginRight: '20px'}} />
             {sessionUser?.email}</button>
           <div className='dropdown-content'>
+            <button
+              className='tickets-dropdown-btn'
+              onClick={() => history.push(`/users/${sessionUser?.id}`)}
+            >Tickets</button>
+            <button
+              className='likes-dropdown-btn'
+            >Likes</button>
+            <button
+              className='settings-dropdown-btn'
+              onClick={() => history.push(`/users/${sessionUser?.id}`)}
+            >Account Settings</button>
             <button onClick={ async () => {
               await dispatch(logout());
               history.push('/landingpage');
             }} className='logout'>Logout</button>
           </div>
         </div>
-        {/* <LogoutButton /> */}
       </div>
     </nav>
   );
