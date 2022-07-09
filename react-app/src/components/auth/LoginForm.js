c17cd28d14cb7f60c158401e14c21bcbb1422ab7
MasonKogami <maskogami@gmail.com>
2022-07-08 22:35:59 -0700

    update login modal with password visibility button

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import { login } from '../../store/session';
+import { FaEyeSlash, FaEye } from 'react-icons/fa';
 import './LoginForm.css';
 
 const LoginForm = ({ closeModalFunc, toggleLoginSignupFunc }) => {
@@ -11,6 +12,7 @@ const LoginForm = ({ closeModalFunc, toggleLoginSignupFunc }) => {
   const user = useSelector(state => state.session.user);
   const dispatch = useDispatch();
   const [loginDisplay] = useState('displayed');
+  const [showPassword, setPasswordVisibility] = useState(false);
 
   const onLogin = async (e) => {
     e.preventDefault();
@@ -32,63 +34,76 @@ const LoginForm = ({ closeModalFunc, toggleLoginSignupFunc }) => {
     return <Redirect to='/home' />;
   }
 
+  const togglePassword = () => {
+
+    setPasswordVisibility(!showPassword);
+  }
+
   const stopTheProp = e => e.stopPropagation();
 
   return (
     <div className={`login-body ${loginDisplay}`} onClick={stopTheProp} onMouseDown={stopTheProp}>
-      <h2 style={{color: '#d1410c', fontWeight: 'bolder', display: 'flex', justifyContent: 'center', fontSize: '35px', marginBottom: '10px'}}>
-        Login <span style={{margin: '0px 10px'}}>or</span> <span className='toggle-signup' onClick={toggleLoginSignupFunc}>Sign Up</span></h2>
+      {/* <h2 style={{fontSize: '18px', height: '22px', marginBottom: '5px', position: 'relative', left: '46px', color: '#d1410c'}}>Eventure</h2> */}
+      <h2 style={{color: '#39364f', fontWeight: 'bolder', fontSize: '35px', marginBottom: '10px', position: 'relative', left: '38px', marginTop: '15px'}}>
+        Log in</h2>
       <form onSubmit={onLogin} className='login-form'>
         <div>
           {errors.map((error, ind) => (
             <div style={{color: '#d1410c'}} key={ind}>{error}</div>
           ))}
         </div>
-        <div>
-          <div style={{marginBottom: '3px'}}>
+        <div className='email-field'>
+          <div style={{marginLeft: '8px'}}>
             <label>
               Email
             </label>
-            <label style={{fontSize: '12px', color: '#d1410c', marginLeft: '8px'}}>
+            {/* <label style={{fontSize: '12px', color: '#d1410c', marginLeft: '8px'}}>
               Required *
-            </label>
+            </label> */}
           </div>
           <input
             name='email'
             type='text'
-            placeholder='Email'
             value={email}
             onChange={updateEmail}
-            style={{height: '20px', width: '250px'}}
+            style={{height: '35px', width: '250px'}}
           />
         </div>
-        <div>
-          <div style={{marginBottom: '3px'}}>
+        <div className='password-field'>
+          <div>
             <label>
               Password
             </label>
-            <label style={{fontSize: '12px', color: '#d1410c', marginLeft: '8px'}}>
+            {/* <label style={{fontSize: '12px', color: '#d1410c', marginLeft: '8px'}}>
               Required *
-            </label>
+            </label> */}
           </div>
           <input
             name='password'
-            type='password'
-            placeholder='Password'
+            type={showPassword ? "text" : "password"}
             value={password}
             onChange={updatePassword}
-            style={{height: '20px', width: '250px'}}
+            style={{height: '35px', width: '250px', position: 'relative'}}
           />
-          <div className='submit-button'>
-            <button className='sub-btn' type='submit'>Login</button>
+          <div onClick={togglePassword} className='eye-btn'>
+            { showPassword? <FaEyeSlash /> : <FaEye /> }
           </div>
         </div>
+        <div className='submit-button'>
+          <button className='sub-btn' type='submit'>Log in</button>
+        </div>
       </form>
+      {/* <button onClick={togglePassword} className='password-vis-btn'>
+        { showPassword? <FaEyeSlash /> : <FaEye /> }
+      </button> */}
+      <div className='or-divider'>or</div>
+      <div className='or-line'></div>
       <div className='demo-button'>
         <button className='demo-btn' onClick={() => dispatch(login('demo@aa.io', 'password'))}>
           Demo User
         </button>
       </div>
+      <div className='toggle-signup' onClick={toggleLoginSignupFunc}>Sign Up</div>
     </div>
   );
 };
