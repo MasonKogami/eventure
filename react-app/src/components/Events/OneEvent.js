8ee1e2a9ee09387ae092b9a5810feab8aa31df4a
MasonKogami <maskogami@gmail.com>
2022-07-13 17:13:45 -0700

    add div containers to control layout of event listing page

--- react-app/src/components/Events/OneEvent.js
+++ react-app/src/components/Events/OneEvent.js
@@ -21,6 +21,7 @@ const OneEvent = () => {
   const { eventId } = useParams();
   const event = useSelector(state => state.events[eventId]);
   const eventHost = event?.user?.username;
+  console.log(eventHost);
   const [showEditModal, setShowEditModal] = useState(false);
   const [showCheckoutModal, setCheckoutModal] = useState(false);
   // console.log(eventHost);
@@ -59,73 +60,83 @@ const OneEvent = () => {
   return (
     <div className='oneevent-page'>
       {/* <div className='bg-image-header'></div> */}
-      <div className='header-div'>
-        {/* <div>
-          <img alt='event-header' src='https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F230834389%2F285623250502%2F1%2Foriginal.20210604-004626?w=600&auto=format%2Ccompress&q=75&sharp=10&rect=0%2C0%2C2160%2C1080&s=f94e8cdbce75497cb7ede588ea34da22' />
-        </div> */}
-        <div style={{fontSize: '40px', color: '#d1410c', fontWeight: 'bolder'}}>
-          {event?.name}
+      <div style={{height: '500px', width: '100%', boxShadow: '0 1px 2px 0 rgb(0 0 0 / 15%)', marginTop: '10px'}}>
+        <div style={{backgroundImage: `url(${event?.image_url})`, filter: 'blur(6.5px)', height: '500px', width: '100%'}}>
         </div>
-        <div style={{marginBottom: '4px'}}>
-          Hosted by {eventHost}
+      </div>
+      <div className='event-listing-div'>
+        <div className='event-listing-details'>
+          <div style={{backgroundImage: `url(${event?.image_url})`, height: '360px', width: '720px', backgroundSize: 'cover'}}>
+            {/* <img alt='event-header' src={`${event?.image_url}`} /> */}
+          </div>
+          <div className='event-details-con'>
+            <div>
+              <div style={{color: '#d1410c'}}>
+                {event?.date.slice(8, 11)} 
+              </div>
+              <span style={{color: '#d1410c'}}>{event?.date.slice(5,7)}</span>
+            </div>
+            <div style={{fontSize: '22px', color: '#1e0a3c'}}>
+              {event?.name}
+            </div>
+            <div style={{marginBottom: '15px', color: '#1e0a3c'}}>
+              By <button className='owner-btn' /* onClick={() => history.push(`/users/${eventHost?.id}`)} */>{eventHost}</button>
+            </div>
+            <div className='event-owner-buttons-con'>
+              {(sessionUser.id === event?.user_id) && (<button className='edit-btn' onClick={showEditModalFunc}>
+                {/* <GoPencil style={{marginRight: '6px'}}/> */}
+                Edit</button>)}
+              {showEditModal && (
+                <Modal inputRef={inputRef} closeModalFunc={closeEditModalFunc} className='edit-event-modal-background'>
+                  <EditEventForm inputRef={inputRef} style={{display: 'flex', justifyContent: 'center'}} closeModalFunc={closeEditModalFunc} />
+                </Modal>
+              )}
+              {(sessionUser.id === event?.user_id) && (<ConfirmationModal 
+                    message="Are you sure you want to delete this event?"
+                    actionButtonLabel="Delete Event"
+                    func={() => deleteOneEvent(event)}
+                  >
+                <button
+                  className='delete-btn'
+                  onClick={() => window.scrollTo(0, document.body.scrollHeight / 3)}
+                >
+                  {/* <AiFillDelete style={{marginRight: '5px', position: 'relative', bottom: '1px'}}/> */}
+                  Delete</button>
+              </ConfirmationModal>
+              )}
+            </div>
+          </div>
         </div>
-        <hr style={{width: '80%'}}></hr>
-        <FaCalendarAlt />
-        <div style={{fontSize: '20px', marginBottom: '8px', color: '#d1410c', fontWeight: 'bolder'}}>
-          Date
-        </div>
-        <div>
-          {event?.date.slice(0, 16)}
-        </div>
-        <div>
-          {(sessionUser.id === event?.user_id) && (<button className='edit-btn' onClick={showEditModalFunc}>
-            <GoPencil style={{marginRight: '6px'}}/>
-            Edit</button>)}
-          {showEditModal && (
-            <Modal inputRef={inputRef} closeModalFunc={closeEditModalFunc} className='edit-event-modal-background'>
-              <EditEventForm inputRef={inputRef} style={{display: 'flex', justifyContent: 'center'}} closeModalFunc={closeEditModalFunc} />
+        <div className='event-listing-ticket-div'>
+          {(sessionUser.id !== event?.user_id) && (<button className='checkout-btn' onClick={showCheckoutModalFunc}>
+            <FaTicketAlt style={{marginRight: '5px', position: 'relative', bottom: '1px'}}/>
+            Tickets</button>)}
+          {showCheckoutModal && (
+            <Modal className='checkoutmodal-background' closeModalFunc={closeCheckoutModalFunc}>
+              <Checkout closeModalFunc={closeCheckoutModalFunc}/>
             </Modal>
           )}
         </div>
-        <div>
-          {(sessionUser.id === event?.user_id) && (<ConfirmationModal 
-                message="Are you sure you want to delete this event?"
-                actionButtonLabel="Delete Event"
-                func={() => deleteOneEvent(event)}
-              >
-            <button
-              className='delete-btn'
-              onClick={() => window.scrollTo(0, document.body.scrollHeight / 3)}
-            >
-              <AiFillDelete style={{marginRight: '5px', position: 'relative', bottom: '1px'}}/>
-              Delete</button>
-          </ConfirmationModal>
-          )}
-        </div>
-        <FaMapPin style={{marginTop: '13px'}}/>
-        <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
-          <div style={{display: 'flex', justifyContent: 'center', marginTop: '1px', fontSize: '20px', color: '#d1410c', fontWeight: 'bolder'}}>
-            Location
-          </div>
-          <div className='event-info'>
-            <div style={{marginBottom: '10px'}}>
-              {event?.location_name}
+        {/* <hr style={{width: '80%'}}></hr> */}
+        <div className='event-listing-about-section'>
+          <FaCalendarAlt />
+          <FaMapPin style={{marginTop: '13px'}}/>
+          <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
+            <div style={{display: 'flex', justifyContent: 'center', marginTop: '1px', fontSize: '20px', color: '#d1410c', fontWeight: 'bolder'}}>
+              Location
             </div>
-            <div>
-              {event?.address}
+            <div className='event-info'>
+              <div style={{marginBottom: '10px'}}>
+                {event?.location_name}
+              </div>
+              <div>
+                {event?.address}
+              </div>
             </div>
-            {(sessionUser.id !== event?.user_id) && (<button className='checkout-btn' onClick={showCheckoutModalFunc}>
-              <FaTicketAlt style={{marginRight: '5px', position: 'relative', bottom: '1px'}}/>
-              Tickets</button>)}
-            {showCheckoutModal && (
-              <Modal className='checkoutmodal-background' closeModalFunc={closeCheckoutModalFunc}>
-                <Checkout closeModalFunc={closeCheckoutModalFunc}/>
-              </Modal>
-            )}
+            <BsFillFileEarmarkTextFill style={{display: 'flex', justifyContent: 'center', marginTop: '12px'}} />
+            <h4 style={{display: 'flex', justifyContent: 'center', color: '#d1410c', marginTop: '10px'}}>About this Event:</h4>
+            <p style={{display: 'flex', justifyContent: 'center', paddingLeft: '55px', paddingRight: '55px', wordBreak: 'break-word', /* marginLeft: 'auto', marginRight: 'auto', */ height: '100%'}}>{event?.description}</p>
           </div>
-          <BsFillFileEarmarkTextFill style={{display: 'flex', justifyContent: 'center', marginTop: '12px'}} />
-          <h4 style={{display: 'flex', justifyContent: 'center', color: '#d1410c', marginTop: '10px'}}>About this Event:</h4>
-          <p style={{display: 'flex', justifyContent: 'center', paddingLeft: '55px', paddingRight: '55px', wordBreak: 'break-word', /* marginLeft: 'auto', marginRight: 'auto', */ height: '100%'}}>{event?.description}</p>
         </div>
       </div>
     </div>
