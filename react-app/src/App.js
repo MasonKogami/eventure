42bdc28cd48542610c74476b0a44d6219a2ad50c
MasonKogami <maskogami@gmail.com>
2022-12-05 13:28:23 -0800

    removed /landingpage, changed to /

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -50,7 +50,7 @@ function App() {
             <User />
           </ProtectedRoute>
           <ProtectedRoute path='/' exact={true} >
-            {sessionUser ? <Redirect to='/home' /> : <Redirect to='/landingpage' />}
+            {sessionUser ? <Redirect to='/home' /> : <Redirect to='/' />}
           </ProtectedRoute>
           <ProtectedRoute path='/home' exact={true} >
             <HomePage />
@@ -69,7 +69,7 @@ function App() {
           <ProtectedRoute path={`/likes`}>
             <Likes />
           </ProtectedRoute>
-          <Route path='/landingpage' exact={true}>
+          <Route path='/' exact={true}>
             <LandingPage />
           </Route>
         </Switch>
