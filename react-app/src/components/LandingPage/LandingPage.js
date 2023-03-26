11cb704483245a3ef93f68b42f8276e7e154a743
MasonKogami <maskogami@gmail.com>
2023-03-25 18:25:42 -0700

    added new sections for the About the Developer and the Tech Used, removed unnecessary images and text

--- react-app/src/components/LandingPage/LandingPage.js
+++ react-app/src/components/LandingPage/LandingPage.js
@@ -58,18 +58,23 @@ const LandingPage = () => {
         </div>
       </div>
       <div className='mission-statement'>
-        <p style={{marginBottom: '0px'}}>Eventure is a global self-service ticketing platform for live experiences that allows anyone to create, share, find and attend events that fuel their passions and enrich their lives.</p>
+        <p style={{marginBottom: '0px'}}>Eventure is a global self-service ticketing platform for live experiences that allows anyone to create, share, find and attend events that fuel their passions and enrich their lives. From music festivals, marathons, conferences, community rallies, and fundraisers, to gaming competitions and air guitar contests. Our mission is to bring the world together through live experiences.</p>
       </div>
       <div>
         <div style={{backgroundImage: 'url(https://cdn.evbstatic.com/s3-build/fe/build/images/7240401618ed7526be7cec3b43684583-2_tablet_1067x470.jpg)', height: '470px', minHeight: '32.4em', minWidth: '100vh', width: '100%', backgroundSize: 'cover'}}/>
       </div>
-      <div className='mission-statement'>
+      {/* <div className='mission-statement'>
         <p style={{marginBottom: '0px'}}>From music festivals, marathons, conferences, community rallies, and fundraisers, to gaming competitions and air guitar contests. Our mission is to bring the world together through live experiences.</p>
-      </div>
-      <div>
+      </div> */}
+      {/* <div>
         <div style={{backgroundImage: 'url(https://cdn.evbstatic.com/s3-build/fe/build/images/6aaf4a36e35b1b71bc077e200ac7429c-1_tablet_1067x470.jpg)', height: '470px', minHeight: '32.4em', minWidth: '100vh', width: '100%', backgroundSize: 'cover'}}/>
-      </div>
+      </div> */}
       {/* <AboutMe style={{position: 'relative', bottom: '0px'}} /> */}
+      <h3>Technology Used:</h3>
+      <section>I utilized React and JavaScript for the frontend with global state management through Redux. I previously learned Python prior to this project so I accompanied the frontend with a Python and Flask backend. For the database, I added PostgreSQL for a multi-version concurrency database for a seamless data read and write. I deployed initially to Heroku prior to some changes in their tiers, it is now deployed with Render. I have also containerized this project with Docker. To top it off, I included an AWS S3 bucket for user-uploaded images, users can upload and it will auto-scale to adjust for additional storage as needed.</section>
+      <h3>About The Developer:</h3>
+      <img />
+      <section>Hi! My name is Mason Kogami. This is my capstone project that I completed during my coding bootcamp at App Academy. I have since graduated and further developed this project and have crafted this project to be special with lots of detail and love.</section>
     </div>
   )
 };
