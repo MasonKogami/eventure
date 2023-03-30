43f4bf5ac793e445e52da9a8572bb63adc4da73c
MasonKogami <maskogami@gmail.com>
2023-03-30 15:08:28 -0700

    removed built with section in about me footer

--- react-app/src/components/AboutMe/AboutMe.js
+++ react-app/src/components/AboutMe/AboutMe.js
@@ -13,7 +13,7 @@ const AboutMe = () => {
       <a target='_blank' rel='noreferrer' href='https://www.linkedin.com/in/masonkogami/'>
         <FaLinkedin size={30} style={{color: '#0a66c2'}}/>
       </a>
-      <p>Built With: </p>
+      {/* <p>Built With: </p>
       <img alt='' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" />
       <img alt='' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg" />
       <img alt='' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" />
@@ -23,7 +23,7 @@ const AboutMe = () => {
       <img alt='' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" />
       <img alt='' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg" />
       <img alt='' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg" />
-      <img alt='' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/heroku/heroku-plain.svg" />
+      <img alt='' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/heroku/heroku-plain.svg" /> */}
     </div>
   );
 };
